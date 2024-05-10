bbe8561e1868f989a7228ba6fc6ce180e16334fb
nexola <vitormatheusfv@gmail.com>
2024-05-10 12:52:58 -0300

    Import sql

---
+++ atividade4/atividade4/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_aluno (nome, curso, idade, matricula) VALUES ('João', 'Química', 23, '12345');
+INSERT INTO tb_aluno (nome, curso, idade, matricula) VALUES ('Vitor', 'ADS', 25, '12346');
+INSERT INTO tb_aluno (nome, curso, idade, matricula) VALUES ('Maria', 'Enfermagem', 20, '12347');
+
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Matemática', 8.5);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Português', 5);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Sociologia', 4);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Física', 9.5);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Geografia', 4);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Química', 5);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Filosofia', 7.5);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Artes', 3);
+INSERT INTO tb_nota (nome_disciplina, nota) VALUES ('Ed. Física', 6);
